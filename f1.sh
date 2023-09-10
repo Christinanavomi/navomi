724f1e64411e3ae422eef731d14398ddbecea5f8
EC2 Default User <christinanavomi24@gmail.com>
2023-09-10 12:30:54 +0000

    edited

--- f1.sh
+++ f1.sh
@@ -3,7 +3,7 @@ echo "enter username"
 read name
 echo "eneter passsword"
 read password
-if [[ $name == "Developer" && $password == "Fireflink@123" ]];
+if [[ $name == admin && $password == testyantra123 ]];
 then 
 	echo "authentication sucessfull"
 else
--- f2.sh
+++ f2.sh
@@ -4,9 +4,9 @@ read sal
 if [[ $sal -lt 15000 ]];
 then 
 	echo "no tax"
-elif [[ $sal -eq 15000 ]];
+elif [[ $sal -gt 15000 && $sal -le 30000 ]];
 then
-        echo "2% tax"
+        echo "5% tax"
 else
 if [[ $sal -gt 30000 ]];
 then 
